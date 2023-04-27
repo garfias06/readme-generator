e934abf8c406fad500aca44eb9643b108f539da5
garfias06 <garfias06_23@outlook.com>
2023-04-27 18:53:22 -0500

    js-code-modified

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license==='None'){
+    return ""
+  }
+  return `![${license}](https://img.shields.io/LICENSE-${license}-blue.svg)`
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,38 +17,47 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}.
+
+  ${renderLicenseBadge(data.license)}
+  
   ## Description
   ${data.description}
+
   ## Table of Contents
-  If your README is long, add a table of contents to make it easy for users to find what they need.
   - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
   - [License](#license)
   - [Features](#features)
-  - [How to contribute](#how to contribute)
+  - [How to contribute](#how-to-contribute)
   - [Tests](#tests)
   - [Questions](#questions)
+
   ## Installation
-  ${data.installation}
+  ${data.installation}.
+
   ## Usage
-  ${data.usage}
+  ${data.usage}.
+
   ## Credits
-  ${data.credits}
+  ${data.credits}.
+
   ## License
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
   ## Features
-  ${data.features}
+  ${data.features}.
+
   ## How to Contribute
-  ${data.contribution}
+  ${data.contribution}.
+
   ## Tests
   To run tests, run the following command:
-  ${data.tests}
+  ${data.tests}.
+
   ##Questions
-  If you have questions about the repository, open an issue or contact me directly at ${data.contactInfo} or GitHub Profile: ${data.github}.
-  `
-  ;
+  If you have questions about the repository, open an issue or contact me directly at ${data.contactInfo} or GitHub Profile: ${data.github}.`
+
 }
 
 module.exports = generateMarkdown;
