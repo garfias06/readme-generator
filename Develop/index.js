c17e53e84807704cf405c60084f7593422408dc1
garfias06 <garfias06_23@outlook.com>
2023-04-26 19:38:08 -0500

    js-file-modified

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util=require('utils')
 
+const readmeFile=require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title for your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Provide a detailed description for your project:',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Required steps to install your project:',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'Provide instructions and examples for use:',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Credits (Collaborators, info resources):',
+    name: 'credits',
+  },
+  {
+    type: 'input',
+    message: 'License:',
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'Features:',
+    name: 'features',
+  },
+  {
+    type: 'input',
+    message: 'How to Contribute:',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'Tests (command to run test):',
+    name: 'tests',
+  },
+  {
+    type: 'input',
+    message: 'Contact (Email): ',
+    name: 'contactInfo',
+  },
+  {
+    type: 'input',
+    message: 'GitHub Profile: ',
+    name: 'github',
+  },
+];
+
+// inquirer
+//   .prompt([])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,44 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Features](#features)
+  - [How to contribute](#how to contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ## Features
+  ${data.features}
+  ## How to Contribute
+  ${data.contribution}
+  ## Tests
+  To run tests, run the following command:
+  ${data.tests}
+  ##Questions
+  If you have questions about the repository, open an issue or contact me directly at ${data.contactInfo} or GitHub Profile: ${data.github}.
+  `
+  ;
 }
 
 module.exports = generateMarkdown;
+
+
+
+
+
+
+
